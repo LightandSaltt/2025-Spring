6b7d597195e3edb080e8c56a9302e121541c5c96
LightandSaltt <qkrtmd5@naver.com>
2025-05-12 11:13:07 +0900

    추가된 내용 커밋

---
+++ springboot-boilerplate/src/test/java/regxp/RegExpTest.java
@@ -0,0 +1,19 @@
+package regxp;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.junit.jupiter.api.Test;
+
+public class RegExpTest {
+
+    // 정규 표현식 : 패턴에 일치하는 문자열을 검색, 치환, 추출하는데 사용하는 표현식
+
+    @Test
+    void testBasic(){
+
+        String str = "spring java javascript html";
+        Pattern regExp = Pattern.compile("script");
+        Matcher matcher = regExp.matcher(str);
+    }
+
+}
